66e449dda204345a8b79053f100e91698bfcdb48
Mike Marin <ww6mm@runbox.com>
2022-12-03 17:59:08 -0800

    added support for Joplin

--- src/ct2md.py
+++ src/ct2md.py
@@ -2,16 +2,23 @@
 # Convert Cherrytree documents to markdown for Zettlr or logseq
 
 import os
+import re
 import sys
 import getopt
 import inspect
 import datetime
 import subprocess
+from shutil import rmtree
 from markdownify import markdownify as mdConvert
 
+# Global variables
 verbose = False
 mdType  = ""
 zettlrID = False
+arRemoveUnderscores = False
+arRemoveNumber = False
+arEmbed = False
+
 mydate = datetime.datetime.now()
 myID = datetime.datetime.now().strftime("%G%m%d")
 myIDcount = 0
@@ -38,7 +45,7 @@ def warn(*args, **kwargs):
     else:
         print("WARNING: ", *args, **kwargs, sep="", file = sys.stderr)
 
-def convert_file(src, dst, tags):
+def convert_file(src, dst, tags, base):
     """Convert the src HTML file to the dst markdown file
 
     Parameters
@@ -47,6 +54,8 @@ def convert_file(src, dst, tags):
         Source full file HTML name
     dst : str
         Destination full file markdown name
+    base : str
+        Base directory for secundary files (for example images)
 
     Returns
     -------
@@ -56,9 +65,30 @@ def convert_file(src, dst, tags):
     global myIDcount
     vbprint("CONVERT:",src,"->",dst, tags)
     try:
+        # Do we need to create a folder?
         dstFolder = os.path.dirname(dst)
         if not os.path.exists(dstFolder):
             os.makedirs(dstFolder)
+
+        # Do we need to embed the file inside a folder?
+        if arEmbed:
+            #print("src:'",src,"', dst:'",dst,"', folder:'",dstFolder)
+            name, ext = os.path.splitext(dst)
+            if os.path.isfile(dstFolder + ".md"):
+                #print("FOLDER:'", dstFolder,"':'",name,"':'",src,"'")
+                fileName = dstFolder + ".md"
+                os.rename(fileName, os.path.join(dstFolder, os.path.basename(fileName)))
+            if os.path.isdir(name):
+                #print("FILE:'", dstFolder,"':'",name,"':'",src,"'")
+                dst = os.path.join(name,os.path.basename(dst))
+
+        # Do we already have a file with the same name?
+        nIncrement = 0
+        while os.path.isfile(dst):
+            nIncrement += 1
+            vbprint("duplicate file:", dst)
+            name, ext = os.path.splitext(dst)
+            dst = name + str(nIncrement) +  ext
 
         flSrc = open(src, 'r')
         html = flSrc.read()
@@ -67,12 +97,12 @@ def convert_file(src, dst, tags):
 
         # Front matter
         fm = ""
-        tagx = [] # Contain the tags with blanks replaced by '-'
-        for t in tags:
-            if " " in t:
-                tagx.append(t) # Let add the spaces too, as I don't know how zettlr handle tags with blanks
-            tagx.append(t.replace(" ","-"))
         if mdType == "z":
+            tagx = [] # Contain the tags with blanks replaced by '-'
+            for t in tags:
+                if " " in t:
+                    tagx.append(t) # Let add the spaces too, as I don't know how zettlr handle tags with blanks
+                tagx.append(t.replace(" ","-"))
             fm += "---\ntitle: " + tags[-1] + "\nkeywords:\n"
             for t in tagx:
                 fm += "  - " + t + "\n"
@@ -82,6 +112,14 @@ def convert_file(src, dst, tags):
                 fm += "ID: " + myID + str(myIDcount).zfill(4) + "\n"
             fm += "---\n"
             fm += "\n# " + " / ".join(tags) + "\n  tags: #" + " #".join(tagx) + "\n"
+
+        if mdType == "j":
+            images = re.findall(r'!\[([^\]]*)\]\(([^\)]+)\)',md)
+            if images:
+                print("IMAGES:",base,images)
+                for i in images:
+                    print(i[0],os.path.isfile(os.path.join(base,i[1])),os.path.join(base,i[1]))
+                    md = md.replace("![" + i[0] + "](" + i[1] + ")","![" + i[0] + "](" + os.path.join(os.getcwd(),base,i[1]) + ")")
 
         flDst = open(dst, 'w')
         flDst.write(fm)
@@ -136,7 +174,8 @@ def traverse_path(arPath, arOutput):
                 # name is the file name without path or extension
                 # ext is the file extension
                 name, ext = os.path.splitext(filename)
-                print("NAME:",name,"EXT",ext)
+
+                #print("NAME:",name,"EXT",ext,"flags",arRemoveNumber,arRemoveUnderscores)
                 # base is the top sub-directory
                 base = os.path.basename(rootDir)
 
@@ -146,9 +185,18 @@ def traverse_path(arPath, arOutput):
 
                 ## B) Process HTML files
                 if ext in [".html", ".HTML"]:
-                    targetName = name + ".md"
                     tags = name.replace("_"," ").split("--")
-                    convert_file(sourceName,os.path.join(arOutput,root,targetName.replace("--",os.sep)),tags)
+
+                    # Generate the correct target name (the name of the output file)
+                    if arRemoveNumber:
+                        name = re.sub("_\d*$","",name)
+                    if arRemoveUnderscores:
+                        name = name.replace("_", " ")
+                    if (not name) or (name.isspace()):
+                        name = "Unknown" # TODO need to generate a better name in this case
+
+                    targetName = name + ".md"
+                    convert_file(sourceName,os.path.join(arOutput,root,targetName.replace("--",os.sep)),tags,arOutput)#os.path.join(arOutput,root))
                 else:
                     clone_file(sourceName,os.path.join(arOutput,root,filename))
 
@@ -181,11 +229,19 @@ def arguments() :
     print("                           If --cherrytree or --CherryTree is used then <path> is overwriten")
     print("                           with the output of the cherrytree export")
     print("   -o, --output  <path>    Output folder with the resulting markdown (default markdown)")
-    print("   -h, --help              Prints this help")
+    print("   -d, --delete            Delete the content of the output folder if present")
+    print("                           (Example: if output folder is markdown, then markdown/* is deleted)")
     print("   -v, --verbose           Produces verbose stdout output")
-    print("   -z, --zettlr            Produces Zettlr markdown")
+    print("   -s, --spaces            replaces underscores (_) in file names with spaces")
+    print("                           (Example: file name 'foo_zoo.html' becomes 'foo zoo.md')")
+    print("   -n, --numbers           remove the numbers appended by CherryTree to the file names")
+    print("                           (Example: file name 'foo_274.html' becomes 'foo.md')")
+    print("   -e, --embed             Embed files withe same name as a folder inside the folder")
     print("   -i, --id                Generates a Zettlr id using YYYYMMDDnnnn where nnnn is sequential")
+    print("   -j, --joplin            Produces Joplin markdown (set -s, -n and -e)")
     print("   -l, --logseq            Produces Logseq markdown")
+    print("   -z, --zettlr            Produces Zettlr markdown")
+    print("   -h, --help              Prints this help")
     sys.exit(2)
 
 def main(argv):
@@ -193,12 +249,13 @@ def main(argv):
    arPath       = "HTML"
    arOutput     = "markdown"
    arInput      = ""
+   arDelete     = False
    arFlatpak    = False
    arCherrytree = False
 
    try:
-       opts, args = getopt.getopt(argv,"hvzlio:p:c:C:",
-               ["help","verbose","zettlr","logseq","id","output=","path=","cherrytree","CherryTree"])
+       opts, args = getopt.getopt(argv,"hvzlijsedno:p:c:C:",
+               ["help","verbose","zettlr","logseq","id","joplin","output=","path=","cherrytree","CherryTree","spaces","numbers","embed","delete"])
    except getopt.GetoptError as e:
       error(e)
       arguments()
@@ -206,15 +263,29 @@ def main(argv):
    global mdType
    global verbose
    global zettlrID
+   global arRemoveUnderscores
+   global arRemoveNumber
+   global arEmbed
+
    for opt, arg in opts:
       if (opt in ("-h","--help")) or (len(sys.argv) == 1):
          arguments()
       elif opt in ("-z", "--zettlr"):
           mdType = "z"
+      elif opt in ("-j", "--joplin"):
+          mdType = "j"
       elif opt in ("-l", "--logseq"):
           mdType = "l"
+      elif opt in ("-n", "--numbers"):
+          arRemoveNumber = True
+      elif opt in ("-s", "--spaces"):
+          arRemoveUnderscores = True
+      elif opt in ("-e", "--embed"):
+          arEmbed = True
       elif opt in ("-i", "--id"):
           zettlrID = True
+      elif opt in ("-d", "--delete"):
+          arDelete = True
       elif opt in ("-v", "--verbose"):
           verbose = True
       elif opt in ("-o", "--output"):
@@ -233,10 +304,31 @@ def main(argv):
           error("Invalid argument")
           arguments()
 
+   if arDelete and os.path.isdir(arOutput):
+       for fl in os.listdir(arOutput):
+           print("DEL:", fl)
+           try:
+               name = os.path.join(arOutput, fl)
+               if os.path.isdir(name):
+                   rmtree(name)
+               else:
+                   os.remove(name)
+           except OSError:
+               vbprint("Unable to delete:",fl)
+               pass
+
    if arCherrytree:
          export_cherrytree(arFlatpak, arInput, arPath)
 
-   print("Converting folder: ",arPath," to ",arOutput)
+   if mdType == "j":
+          arEmbed = True
+          arRemoveNumber = True
+          arRemoveUnderscores = True
+
+   if not os.path.isdir(arPath):
+       arguments()
+
+   print("Converting folder: `",arPath,"' to `",arOutput,"'")
    traverse_path(arPath, arOutput)
    print("done")
 
